5b48085008bdfa2ab1e0a697f1703ed838c3b615
Jeongho Mok <jelvjh@gmail.com>
2024-11-10 23:59:44 +0900

    nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,23 +1,25 @@
 return {
   {
-    "nvim-lspconfig",
+    "neovim/nvim-lspconfig",
     opts = {
       diagnostics = {
         virtual_text = false,
       },
       servers = {
+        -- pyright = {}
         basedpyright = {
           settings = {
             python = {
               analysis = {
                 typeCheckingMode = "off",
-                diagnosticSeverityOverrides = {
-                  -- reportUnknownVariableType = "none",
-                  reportUnknownVariableType = "false",
-                  reportUnknownMemberType = false,
-                  reportUnknownParameterType = false,
-                  reportMissingParameterType = false,
-                },
+                -- diagnosticSeverityOverrides = {
+                --   -- reportUnknownVariableType = "none",
+                --   reportUnknownVariableType = "false",
+                --   reportUnknownMemberType = false,
+                --   reportUnknownParameterType = false,
+                --   reportMissingParameterType = false,
+                --   -- reportDeprecated = false,
+                -- },
               },
             },
           },
