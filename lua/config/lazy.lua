a7ce7f8c38216d0714b1eb181fed463ba3ae52ae
Jeongho Mok <jelvjh@gmail.com>
2024-11-19 23:21:20 +0900

    add copilot

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,6 +32,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
+    { import = "lazyvim.plugins.extras.ai.copilot" },
+    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
+    { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "plugins" },
   },
   defaults = {
